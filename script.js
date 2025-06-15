7a3a3b3fbb938c770a7220b02884f8b43d75eb32
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-06-15 17:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
-    //Write your code here
-
-
+  const strongElements = document.querySelectorAll('strong');
+  strongElements.forEach(el => {
+    el.style.color = 'rgb(0, 128, 0)';
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  const strongElements = document.querySelectorAll('strong');
+  strongElements.forEach(el => {
+    el.style.color = 'rgb(0, 0, 0)';
+  });
 }
